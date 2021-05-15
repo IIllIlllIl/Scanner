601ef78685a27aacd8a63f3d5e21f167deda1192
IIllIlllIl <wang.tr@outlook.com>
2021-05-15 10:51:49 +0800

    5.15

---
+++ main.py
@@ -0,0 +1,26 @@
+#
+# wang.tr@outlook.com
+#
+
+# global var
+output = ()
+error = ()
+line = 0
+err = 1
+c = 0
+
+
+def val():
+    global c
+    c = 5
+
+
+def read_file():
+    pass
+
+
+if __name__ == '__main__':
+    pass
+
+
+
