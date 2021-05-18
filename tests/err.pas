e8dd3d6b73928041b575665b4fd17a8b2188a34e
IIllIlllIl <wang.tr@outlook.com>
2021-05-18 13:20:33 +0800

    5.18

--- tests/err.pas
+++ tests/err.pas
@@ -1,10 +1,10 @@
 begin
-  integer k;
+  integer k01234567890123456789;
   integer function F(n);
     begin
       integer n;
       if n<=0 then F:=1
-      else F:=n*F(n-1)
+      else $F:n*F(n-1)
     end;
   read(m);
   k:=F(m);
